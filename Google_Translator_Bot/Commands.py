b7d4201ce7bfd2839a54d1881457e2ce59786fcb
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-06-19 23:59:43 +0400

    update Commands.py

--- Google_Translator_Bot/Commands.py
+++ Google_Translator_Bot/Commands.py
@@ -44,7 +44,6 @@ async def echo(client, message):
         quote = True
     )
 
-
 import datetime
 import motor.motor_asyncio
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
@@ -68,13 +67,13 @@ from pyrogram.errors import (
 
 ############## DEĞİŞKENLER ##############
 
-DATABASE_URL = os.environ.get("DATABASE_URL") # MongoDB veritabanınızın url'si. Nasıl alacağınızı bilmiyorsanız destek grubu @RepoHaneX'e gelin.
+DATABASE = os.environ.get("DATABASE") # MongoDB veritabanınızın url'si. Nasıl alacağınızı bilmiyorsanız destek grubu @RepoHaneX'e gelin.
 BOT_USERNAME = os.environ.get("BOT_USERNAME") # Botunuzun kullanıcı adı.
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL")) # Botunuzun eylemleri kaydedeceği kayıt grubunun id'si.
-GROUP_SUPPORT = os.environ.get("GROUP_SUPPORT", "RepoHaneX") # Botunuzdan yasaklanan kullanıcıların itiraz işlemleri için başvuracağı grup, kanal veya kullanıcı. Boş bırakırsanız otomatik olarak OWNER_ID kimliğine yönlendirecektir.
+GROUP_SUPPORT = os.environ.get("GROUP_SUPPORT", "teamabasofsupport1") # Botunuzdan yasaklanan kullanıcıların itiraz işlemleri için başvuracağı grup, kanal veya kullanıcı. Boş bırakırsanız otomatik olarak OWNER_ID kimliğine yönlendirecektir.
 GONDERME_TURU = os.environ.get("GONDERME_TURU", False) # Botunuzun yanıtladığınız mesajı gönderme türü. Eğer direkt iletmek isterseniz False, kopyasını göndermek isterseniz True olarak ayarlayın.
-OWNER_ID = int(os.environ.get("OWNER_ID")) # Sahip hesabın id'si
-LANGAUGE = os.environ.get("LANGAUGE", "TR")
+DEV_ID = int(os.environ.get("DEV_ID")) # Sahip hesabın id'si
+LANGAUGE = os.environ.get("LANGAUGE", "AZ")
 
 
 
